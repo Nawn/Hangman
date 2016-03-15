69c6f36b4d42cd20f4783943cbefe690bff8dd6b
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-03-14 19:18:11 -0700

    Functional Game, need to implement Save and Load

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 class Hangman
+	attr_reader :guesses_left, :answer
 	#Class Method, to pick a random word
 	def self.select_random_word_from(filename, min_size=5, max_size=12)
 		potential_words = []
@@ -20,17 +21,33 @@ class Hangman
 
 	#Display Data to screen
 	def display
-		puts "Incorrect Guesses Left: #{@guesses_left}"
-		puts "Incorrect Letters: #{@incorrect_letters}"
+		puts "\n\nIncorrect Guesses Left: #{@guesses_left}"
+		puts "Incorrect Guesses: #{@incorrect_letters.join(", ")}"
 		puts "Progress: #{@solved.join(" ")}" #Join the array with Spaces, so it's readable
 	end
 
-	def test
-		puts "Testing @answer: #{@answer}"
-		puts "Testing @solved: #{@solved.join(" ")}" #Join the array with Spaces, so it's readable
+	def test;	end
+
+	def turn(input_char)
+		#Raise error if the input is not valid
+		raise(StandardError, "1 Letter at a time!!") unless input_char.length == 1 || input_char.downcase == "save" || input_char.downcase == "exit"
+		exit if input_char.downcase == 'exit'#Exit game if they type 'exit' this turn
+
+		if @answer.split("").any? {|letter| letter == input_char} #If the input letter exists in the answer
+			@answer.split("").each_with_index do |letter, index| #Go through the answer
+				@solved[index] = input_char if letter == input_char #And set the letter in the solved array
+			end
+		elsif input_char.downcase == "save" #save if they type "save"
+			puts "\n\nNot implemented yet!" 
+		else #if they're not exitting, saving, or putting a correct letter
+			@incorrect_letters << input_char #Add the letter they tried to an array of incorrect letters
+			@guesses_left -= 1 #Then subtract an available guess
+		end
 	end
 
-	def turn; end
+	def over?
+		!@solved.include?("_") #If there are any underscores left, the game is not done.(Return False)
+	end
 
 	private
 	#returns an array of underscores based 
--- play.rb
+++ play.rb
@@ -7,3 +7,16 @@ puts "Not implemented yet" if response.downcase == "load"
 exit if response.downcase == "exit" || response.downcase == "load"
 
 game = Hangman.new
+
+until game.guesses_left <= 0 || game.over?
+  begin
+    game.display
+    game.turn(gets.chomp)
+  rescue StandardError => e
+    puts "\n\nError: #{e}"
+    retry
+  end
+end
+
+puts "You Guessed it! The word was '#{game.answer}'" if game.over?
+puts "You ran out of guesses! The word was '#{game.answer}'" if game.guesses_left <= 0
